bbe16371ef8d61dfa21f85cbd04ee2d8d0f4981f
lkun <lkungithub@163.com>
2014-03-23 10:55:04 +0800

    date

--- tb-common-utils/tbnet/src/iocomponent.cpp
+++ tb-common-utils/tbnet/src/iocomponent.cpp
@@ -20,25 +20,28 @@ namespace tbnet {
 /*
  * 构造函数
  */
-IOComponent::IOComponent(Transport *owner, Socket *socket) {
-    assert(socket);
-    _owner = owner;
-    _socket = socket;
-    _socket->setIOComponent(this);
-    _socketEvent = NULL;
-    atomic_set(&_refcount, 0);
-    _state = TBNET_UNCONNECTED; // 正在连接
-    _autoReconn = false; // 不要自动重连
-    _prev = _next = NULL;
-    _lastUseTime = tbsys::CTimeUtil::getTime();
-    _inUsed = false;
+IOComponent::IOComponent(Transport *owner, Socket *socket)
+{
+	assert(socket);
+	_owner = owner;
+	_socket = socket;
+	_socket->setIOComponent(this);
+	_socketEvent = NULL;
+	atomic_set(&_refcount, 0);
+	_state = TBNET_UNCONNECTED; // 正在连接
+	_autoReconn = false; // 不要自动重连
+	_prev = _next = NULL;
+	_lastUseTime = tbsys::CTimeUtil::getTime();
+	_inUsed = false;
 }
 
 /*
  * 析构函数
  */
-IOComponent::~IOComponent() {
-    if (_socket) {
+IOComponent::~IOComponent() 
+{
+    if (_socket) 
+	{
         _socket->close();
         delete _socket;
         _socket = NULL;
